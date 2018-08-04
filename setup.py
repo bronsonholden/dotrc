620caebcbbedb1d56667d0544758245e81f3a3c8
Paul Holden <pholden@stria.com>
2018-08-04 02:35:00 -0700

    More package details in setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,13 @@ from setuptools import setup
 
 setup(
     name='dotrc',
-    version='0.1dev',
+    description='Simple .rc file loading for your Python projects',
+    author='Paul Holden',
+    author_email='pholden@stria.com',
+    version='0.1dev1',
     packages=['dotrc'],
     package_dir={'': 'src'},
     license='MIT',
-    install_requires=['PyYAML','simplejson']
+    install_requires=['PyYAML','simplejson'],
+    long_description=read('README.md')
 )
