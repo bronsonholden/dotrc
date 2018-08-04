4bcce4a19b76369a227137939f6249c3d90e1cab
Paul Holden <pholden@stria.com>
2018-08-04 02:37:36 -0700

    Add URL (guess, may have to fix)

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
+import os
 from setuptools import setup
+
+def read(filename):
+    return open(os.path.join(os.getcwd(), filename)).read()
 
 setup(
     name='dotrc',
@@ -8,6 +12,7 @@ setup(
     version='0.1dev1',
     packages=['dotrc'],
     package_dir={'': 'src'},
+    url='https://pypi.org/project/dotrc/',
     license='MIT',
     install_requires=['PyYAML','simplejson'],
     long_description=read('README.md')
