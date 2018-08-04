7d71ce612664f45176de73adf4c7efe94418365c
Paul Holden <pholden@stria.com>
2018-08-04 02:40:22 -0700

    Add long desc content type

--- setup.py
+++ setup.py
@@ -9,11 +9,12 @@ setup(
     description='Simple .rc file loading for your Python projects',
     author='Paul Holden',
     author_email='pholden@stria.com',
-    version='0.1dev1',
+    version='0.1dev2',
     packages=['dotrc'],
     package_dir={'': 'src'},
     url='https://pypi.org/project/dotrc/',
     license='MIT',
     install_requires=['PyYAML','simplejson'],
-    long_description=read('README.md')
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown'
 )
